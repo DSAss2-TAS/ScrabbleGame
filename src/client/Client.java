0e131df8131196cbcb85343af4d5212c406cf90a
AvelineChen <43591250+AvelineChen@users.noreply.github.com>
2018-10-01 18:58:02 +1000

    delete comment

--- src/client/Client.java
+++ src/client/Client.java
@@ -7,8 +7,7 @@ public class Client {
 	public static void main(String[] args) {
 		// TODO main of client Auto-generated method stub
 		MainFrame.getInstance().connectionStartUp();
-		//github Test
-		System.out.println("Git Test");
+
 	}
 
 }
