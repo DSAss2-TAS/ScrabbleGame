fc95db96a4dd64326049df8ccddd2316adf77293
AvelineChen <xuanc5@student.unimelb.edu.au>
2018-10-01 18:53:44 +1000

    Git Test

--- src/client/Client.java
+++ src/client/Client.java
@@ -7,7 +7,8 @@ public class Client {
 	public static void main(String[] args) {
 		// TODO main of client Auto-generated method stub
 		MainFrame.getInstance().connectionStartUp();
-		
+		//github Test
+		System.out.println("Git Test");
 	}
 
 }
